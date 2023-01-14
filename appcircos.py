cc02e5b52cb0c7f1724ee2a76a8ee2e6b9894e50
Leonina013 <118098471+Leonina013@users.noreply.github.com>
2023-01-14 20:49:57 +0530

    Update appcircos.py

--- appcircos.py
+++ appcircos.py
@@ -53,7 +53,7 @@ if file is not None:
         y_col = st.selectbox("Select the column for the Y axis", data.columns)
    
     # Create the histogram
-        plt.hist([data[x_col1], data[x_col2], data[x_col3], data[y_col]], stacked=True, label=[x_col1, x_col2, x_col3])
+        plt.hist([data[x_col1], data[x_col2], data[x_col3]], data[y_col], stacked=True, label=[x_col1, x_col2, x_col3])
         plt.xlabel("X axis columns")
         plt.ylabel(y_col)
         plt.legend()
