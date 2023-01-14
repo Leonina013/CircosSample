d1264f7a7702d77298738303d85674a9a0fbd151
Leonina013 <118098471+Leonina013@users.noreply.github.com>
2023-01-14 21:01:03 +0530

    Update appcircos.py

--- appcircos.py
+++ appcircos.py
@@ -56,6 +56,7 @@ if file is not None:
         plt.bar(data.index, data[x_col1], label=x_col1, width=0.2)
         plt.bar(data.index + 0.2, data[x_col2], label=x_col2, width=0.2)
         plt.bar(data.index + 0.4, data[x_col3], label=x_col3, width=0.2)
+        plt.bar(data[y_col])
         plt.xlabel("Data Label")
         plt.ylabel(y_col)
         plt.legend()
