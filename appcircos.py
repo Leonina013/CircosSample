629f1ed2d1aa57f19d83dd0a49fe5e346a6f3724
Leonina013 <118098471+Leonina013@users.noreply.github.com>
2023-01-14 20:18:58 +0530

    Update appcircos.py

--- appcircos.py
+++ appcircos.py
@@ -32,7 +32,11 @@ if file is not None:
         # Get the columns to use for the X and Y axis
         x_col = st.selectbox("Select the column for the X axis", data.columns)
         y_col = st.selectbox("Select the column for the Y axis", data.columns)
-        st.line_chart(data[[x_col, y_col]])
+         plt.plot(data[x_col], data[y_col])
+         plt.xlabel(x_col)
+         plt.ylabel(y_col)
+         st.pyplot()
+
     elif viz_type == "Scatter Plot":
         # Get the columns to use for the X and Y axis
         x_col = st.selectbox("Select the column for the X axis", data.columns)
